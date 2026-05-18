41e123316339638bff6528697ad19eed6b0b35ec
dev-caiodias <dev.caiodias19@gmail.com>
2026-05-18 13:43:11 -0300

    feat: setup inicial do projeto com estrutur de pacotes

---
+++ src/main/java/com/biblioteca/inclusiva/InclusivaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.inclusiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InclusivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/inclusiva/InclusivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.inclusiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclusivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
